fde615b2368c7b3a919645de9cf8c07954cd346f
Drew Taylor <drew@infiniteinternet.ca>
2021-11-11 10:33:41 -0500

    scaffold

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.svg">
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { QueryClient, setupAuthExtension } from "@cosmjs/stargate";
+import { Tendermint34Client } from "@cosmjs/tendermint-rpc";
+
+const RPC = "https://rpc.constantine-1.archway.tech:443";
+const ContractAddress = "archway1vw9za2nv564rymxdunj5frwwuegpnv90uf6duw";
+
+const Alice = {
+  mnemonic: "enlist hip relief stomach skate base shallow young switch frequent cry park",
+  address0: "arhcway14qemq0vw6y3gc3u3e0aty2e764u4gs5lndxgyk",
+  address1: "archway1hhg2rlu9jscacku2wwckws7932qqqu8xm5ca8y",
+  address2: "archway1xv9tklw7d82sezh9haa573wufgy59vmwnxhnsl",
+  address3: "archway17yg9mssjenmc3jkqth6ulcwj9cxujrxxg9nmzk",
+  address4: "archway1f7j7ryulwjfe9ljplvhtcaxa6wqgula3nh873j",
+};
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data: () => ({
+    contract: ContractAddress,
+    counter: null,
+    queryClient: null,
+    rpc: RPC,
+    tendermintClient: null,
+    user: Alice
+  }),
+  mounted: async function () {
+    await this.init();
+  },
+  methods: {
+    init: async function () {
+      this.tendermintClient = await Tendermint34Client.connect(this.rpc);
+      this.queryClient = QueryClient.withExtensions(this.tendermintClient, setupAuthExtension);
+      console.log('queryClient', this.queryClient);
+    },
+    getCount: async function () {
+      
+    },
+    incrementCounter: async function () {
+
+    },
+    resetCounter: async function () {
+
+    }
   }
 }
 </script>
